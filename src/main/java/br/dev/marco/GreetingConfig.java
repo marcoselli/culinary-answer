c655fd2c6090e40cbf6227f84db3a5c931467372
marcoselli <marco_selli@hotmail.com>
2023-08-08 19:59:33 -0300

    base project

---
+++ src/main/java/br/dev/marco/GreetingConfig.java
@@ -0,0 +1,12 @@
+package br.dev.marco;
+
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithName;
+
+@ConfigMapping(prefix = "greeting")
+public interface GreetingConfig {
+
+    @WithName("message")
+    String message();
+
+}
